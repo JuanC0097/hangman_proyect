aede1211321d04c78e12faa81ca3222d3ca5589a
Juan Carlos Villar Quintero <juanvillarq0097@gmail.com>
2022-07-12 18:27:48 -0500

    creacion de la variable que albergara el bloque de codigo de arte ascii. para manipular,segun se necesite

---
+++ hangman.py
@@ -0,0 +1,16 @@
+from interfaz import dict_ascii
+
+def categorie():
+    
+    interfaz = dict_ascii.get("intr_inicial")
+    interfaz1 = interfaz[1]
+    print(interfaz1)
+    
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    categorie()
+    
--- interfaz.py
+++ interfaz.py
@@ -1,5 +1,64 @@
+import os
+
+def clear():
+    os.system("clear")
+
+
+dict_ascii= { 
+    "intr_inicial": ['''
+            ##########################################################################################   
+            # __                                                                                     #
+            #| |                                                                                     #
+            #| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __               __ _  __ _ _ __ ___  _ _ _  #
+            #| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \   _______   / _` |/ _` | '_ ` _ \|  _ _) #
+            #| | | | (_| | | | | (_| | | | | | | (_| | | | |  |_/_/_/|   (_| | (_| | | | | | | ( )   #
+            #|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|            \__, |\__,_|_| |_| |_|_____) #
+            #                    __/ |                                   __/ |                       #
+            #                   |___/                                   |___ |                       #
+            ##########################################################################################
+                    ''','''
+
+             ########################################################################################
+             #                               Reglas del Juego                                       #
+             #                                                                                      #
+             #               1.No Puedes ingresar numeros                                           #
+             #                                                                                      #
+             #               2.Solo puedes ingresar un caracter                                     #
+             #                                                                                      #
+             #               3.Por favor ingresar los caracteres en mayuscula                       #
+             #                                                                                      #
+             #          4.El juego terminara cuando encuentres la palabra o agotes tus intentos     #
+             #                                                                                      #
+             ########################################################################################
+            
+            ''']
+    
+            }
+
+
+
 def run():
-    print("Hola mundo")
+    clear()
+    print()
+
+    print("Las Categorias son:"+"\n1.FACIL\n2.NORMAL\n3.DIFICIL")
+    categoria = int(input("Por favor elija la categoria: "))
+    if categoria == 1:
+        print("Ha seleccionado la categoria FACIL")
+        categoria == int(categoria)
+        print(categoria)
+    elif categoria == 2:
+        print("Ha seleccionado la categoria NORMAL")
+        categoria == int(categoria)
+        print(categoria)
+    elif categoria == 3:
+        print("Ha seleccionado la categoria DIFICIL")
+        categoria == int(categoria)
+        print(categoria)
+    else:
+        print("No ha seleccionado una categoria")
+
+
 
 if __name__ == '__main__':
     run()
